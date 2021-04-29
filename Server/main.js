62f9f93d1f9fd72fd2732034a52ac62bc1a9a8a6
Jonas <jnsaph@icloud.com>
2021-04-29 19:25:58 +0200

    Session get deleted from Array after they close

--- Server/main.js
+++ Server/main.js
@@ -8,28 +8,7 @@ app.use(express.static('public'));
 app.use(cors())
 
 // Initialized Socket Server
-const wss = new WebSocket.Server({
-    port: 1213,
-    perMessageDeflate: {
-        zlibDeflateOptions: {
-            // See zlib defaults.
-            chunkSize: 1024,
-            memLevel: 7,
-            level: 3
-        },
-        zlibInflateOptions: {
-            chunkSize: 10 * 1024
-        },
-        // Other options settable:
-        clientNoContextTakeover: true, // Defaults to negotiated value.
-        serverNoContextTakeover: true, // Defaults to negotiated value.
-        serverMaxWindowBits: 10, // Defaults to negotiated value.
-        // Below options specified as default values.
-        concurrencyLimit: 10, // Limits zlib concurrency for perf.
-        threshold: 1024 // Size (in bytes) below which messages
-        // should not be compressed.
-    }
-});
+const wss = new WebSocket.Server({port: 1213});
 
 var connections = [];
 
@@ -40,13 +19,18 @@ wss.on('connection', function connection(ws) {
     connections[ws.id] = ws;
 
     // Log and send Sessions
-    console.log("New Connection with sessionID: " + ws.id);
-    ws.send(`You're connected to the Server! \nSession ID: ${ws.id}. Give the SessionID to the Presenters`);
+    console.log("New Connection with Session ID: " + ws.id);
+    ws.send(`You're connected to the Server! \nSession ID: ${ws.id}. Give the Session ID to the Presenters`);
+
+    // Detect if Client Disconnectes and remove ID from Array
+    ws.on('close', () => {
+        console.log("Closed Session with Session ID " + ws.id);
+        delete connections[ws.id];
+    })
+    
 });
 
-app.get('/', (req, res) => {
-
-})
+app.get('/');
 
 app.get('/authenticate/:sessionID', (req, res) => {
     if (!connections[req.params.sessionID]) return res.status(400).send({code: 400, message: "Session does not exist."});
